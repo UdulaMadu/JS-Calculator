1d2d38db9f44c8a5d58532d3023ccfdf5cc4f47d
JayathJS <JayathJSgit@users.noreply.github.com>
2021-09-15 13:08:24 +0530

    done all the functioning parts in js
    using eval() to get the equal value

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function insertData(num)
   
     document.form.textView.value = document.form.textView.value + num
 }
+
+function clean()
+{
+    document.form.textView.value=""
+}
+
+function equal()
+{
+    var exp = document.form.textView.value;
+
+    if(exp)
+    {
+        document.form.textView.value = eval(exp)
+        //eval() is a function property of the global object
+    }
+}
